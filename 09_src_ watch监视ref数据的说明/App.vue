d891e4b214818213f317b32e59ad201a36416215
Ayo11111 <1299124551@qq.com>
2022-06-16 20:29:39 +0800

    watch监视ref数据的说明

---
+++ 09_src_ watch监视ref数据的说明/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <demo></demo>
+</template>
+
+<script>
+import demo from "./components/Demo.vue";
+export default {
+  name: "App",
+  components: {
+    demo,
+  },
+};
+</script>
+
+<style></style>
