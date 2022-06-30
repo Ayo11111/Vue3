4e845e238ea5511b367b933eb964fdf73f2c134d
Ayo11111 <1299124551@qq.com>
2022-06-30 20:47:41 +0800

    readonly和shallowReadonly

---
+++ 15_src_readonly和shallowReadonly/main.js
@@ -0,0 +1,13 @@
+// vue2引入的是Vue构造函数，vue3引入一个createAPP的工厂函数
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+// 创建应用实例对象-app（类似于之前vue2中的vm，但是app比vm更轻）
+const app = createApp(App)
+
+// 挂载 
+app.mount('#app')
+
+// console.log(app);
